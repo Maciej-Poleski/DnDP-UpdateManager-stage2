f594daf6611c90606ac46426e1b94e7f0c6db1ab
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2011-07-10 23:49:27 +0200

    Implementacja stage2 dla Unixa zgodnie z Single Unix Specification v4 i X/Open System Interface. Kod wykonuje swoje podstawowe zadanie. Wymaga testowania szczególnie na platformie Linux x86. To nie jest przyk³ad dobrego kodu. Ca³osæ zostanie jak najszybciej przebudowana i zmodularyzowana - prawdopodobnie kod dla DnDP 0.3 bêdzie napisany od nowa i bêdzie znacznie szczelniejszy.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,31 @@
 using namespace std;
 
 #else
+/* ¯¹dam X/Open System Interface */
+#define _XOPEN_SOURCE 700
+#include <cstdlib>
+#include <cerrno>
+#include <unistd.h>
+#include <cstdio>
+#include <cstring>
+#include <libgen.h>
+#include <spawn.h>
+#include <sys/wait.h>
 
+/* Sprawdzam, czy moje ¿¹danie zostan¹ spe³nione */
+#if _XOPEN_VERSION != 700
+#error Ten kompilator nie jest zgodny z XSI SUSv4
+#error Nie mogê kontynuowaæ
 #endif
 
-int main(int argc,char**argv)
+/* Sprawdzam, czy posix_spawn() jest dostêpna */
+#if !defined(_POSIX_SPAWN)
+#error Ten kompilator nie obs³uguje posix_spawn()
+#error Nie mogê kontynuowaæ
+#endif
+#endif
+
+int main(int argc,char**argv,char**envp)
 {
 #ifdef _WIN32
 	char *fullPath=_fullpath(NULL,argv[0],0);
@@ -38,6 +59,39 @@ int main(int argc,char**argv)
 
 	return 0;
 #else
+	char * path=realpath(argv[0],NULL);
+	char *tmppath=dirname(path);
+	strcpy(path,tmppath);
+	const char* updateManager_jar="/lib/UpdateManager.jar";
+	char *jar=new char[strlen(path)+sizeof(updateManager_jar)];
+	strcpy(jar,path);
+	strcat(jar,updateManager_jar);
 
+#ifdef __LP64__
+	char* bits="x86_64";
+#else
+	char* bits="i386";
+#endif
+
+	pid_t proc1;
+	char* const argv2[] =
+	{		"java",
+			"-cp",
+			jar,
+			"org.dndp.UpdateManager.Launcher",
+			path,
+			bits,
+			NULL
+	};
+
+	if(posix_spawnp(&proc1, "java",NULL,NULL,argv2,envp))
+	    return 3;
+
+	int state;
+	waitpid(proc1,&state,0);
+
+	delete [] jar;
+
+	return 0;
 #endif
 }
